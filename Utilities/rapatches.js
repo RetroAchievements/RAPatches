bf7e443b1ed87778de66fbc7429325c0881f97d0
televandalist <45054151+televandalist@users.noreply.github.com>
2023-01-26 14:33:05 -0600

    Create rapatches.js
    TamperMonkey Script made by wholee https://github.com/wholee - the script adds link(s) to RA entries' linked hashes pages to download patches from this repo.

---
+++ Utilities/rapatches.js
@@ -0,0 +1,119 @@
+// ==UserScript==
+// @name         RAPatches
+// @namespace    https://retroachievements.org
+// @version      0.4
+// @description  Add RAPatches download links to retroachievements.org linked hashes page e.g. https://retroachievements.org/linkedhashes.php?g=19339
+// @author       wholee
+// @match        https://retroachievements.org/linkedhashes.php?g=*
+// @icon         https://static.retroachievements.org/assets/images/favicon.webp
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const RAgitRepo = 'https://api.github.com/repos/televandalist/RAPatches/git/trees/main?recursive=1';
+    const baseLink = 'https://github.com/RetroAchievements/RAPatches/raw/main/';
+
+    const updateInterval = 86400000; // 1 day
+    const currentUnixTimestamp = Date.parse(Date(new Date()));
+
+    var RAgitRepo_lastUpdated = localStorage.getItem("RAgitRepo_lastUpdated");
+    var RAgameID = new URLSearchParams(window.location.search).get('g');
+
+    if (localStorage.getItem("RAgitRepo_lastUpdated") === null ||
+        localStorage.getItem("RAgitRepo_lastUpdated") < currentUnixTimestamp - updateInterval ||
+        localStorage.getItem("RAgitRepo_lastUpdated") === 'NaN'){
+
+        fetch(RAgitRepo)
+            .then(res => res.json())
+            .then(out => {
+            injectRAPatches( RAgameID, getGameIdAndPath(out) );
+            localStorage.setItem('RAgitRepo', JSON.stringify(getGameIdAndPath(out)));
+            localStorage.setItem('RAgitRepo_lastUpdated', currentUnixTimestamp );
+        })
+            .catch(err => { throw err });
+    } else {
+
+        injectRAPatches( RAgameID, JSON.parse(localStorage.getItem("RAgitRepo")) );
+
+    }
+
+    function injectRAPatches( gameId, gameData ){
+
+        var para = document.querySelector("#fullcontainer > p")
+
+        try{
+
+            if(gameData[gameId] === undefined){
+
+                //para.insertAdjacentHTML( 'afterend', '</br>Patches available at RAPatches: N/A</br>' )
+                // Do nothing for now
+
+            } else {
+
+                var injectHTML = '</br><p class="embedded">Patches available at <a href="https://github.com/RetroAchievements/RAPatches">https://github.com/RetroAchievements/RAPatches</a>:</br></br>'
+
+                for (var i = 0; i < gameData[gameId].length; i++) {
+
+                    var pathArray = gameData[gameId][i].split('/');
+                    var zipName = pathArray[pathArray.length - 1]
+
+                    injectHTML = injectHTML + '<a href="' + baseLink + gameData[gameId][i] + '">' + zipName + '</a></br>';
+                }
+
+                injectHTML = injectHTML + '</p>';
+                para.insertAdjacentHTML( 'afterend', injectHTML);
+
+            }
+
+        } catch(error) {
+
+            //para.insertAdjacentHTML( 'afterend', '</br>RAPatches: N/A</br>' )
+            // Do nothing for now
+            alert(error);
+
+        }
+
+    }
+
+    function getGameIdAndPath( RAgitRepoJson ) {
+
+        var gitTree = RAgitRepoJson.tree;
+
+        var gameIdAndPathList = {};
+
+        for (var i = 0; i < gitTree.length; i++){
+
+            var fileObj = gitTree[i];
+
+            for (var key in fileObj){
+
+                if(String(fileObj[key]).endsWith('.zip')){
+
+                    // split path to get the gameID
+                    var pathArray = fileObj[key].split('/');
+                    var zipName = pathArray[pathArray.length - 1]
+
+                    var splitZipNameArray = zipName.split('-');
+                    var gameId = splitZipNameArray[0];
+
+                    if(gameIdAndPathList[gameId] === undefined){
+
+                        var files = []
+                        files.push(fileObj[key]);
+                        gameIdAndPathList[gameId] = files;
+
+                    }else{
+
+                        gameIdAndPathList[gameId].push(fileObj[key]);
+
+                    }
+                }
+            }
+        }
+
+        return gameIdAndPathList;
+    }
+
+})();
